3c1f85c4444ff6affc94b1bb55a195af07dd70de
Adriano Kretschmer <a.kretschmer@cinq.com.br>
2015-04-24 07:40:29 -0300

    Added event to widget view.

--- app/src/main/java/com/dersommer/honestwifiswitch/WifiSwitch.java
+++ app/src/main/java/com/dersommer/honestwifiswitch/WifiSwitch.java
@@ -1,5 +1,6 @@
 package com.dersommer.honestwifiswitch;
 
+import android.app.PendingIntent;
 import android.appwidget.AppWidgetManager;
 import android.appwidget.AppWidgetProvider;
 import android.content.BroadcastReceiver;
@@ -8,6 +9,7 @@ import android.content.Context;
 import android.content.Intent;
 import android.content.IntentFilter;
 import android.net.wifi.WifiManager;
+import android.widget.Button;
 import android.widget.RemoteViews;
 
 
@@ -17,6 +19,8 @@ import android.widget.RemoteViews;
 public class WifiSwitch extends AppWidgetProvider {
 
     BroadcastReceiver receiver;
+
+    private static final String WIFI_CLICKED = "myOnClick";
 
     @Override
     public void onUpdate(Context context, AppWidgetManager manager, int[] appWidgetIds) {
@@ -47,16 +51,18 @@ public class WifiSwitch extends AppWidgetProvider {
                 }
             }
         };
+
         context.getApplicationContext().registerReceiver(receiver, filter);
+
     }
 
     @Override
     public void onDisabled(Context context) {
         // Enter relevant functionality for when the last widget is disabled
         super.onDisabled(context);
-        if(receiver!=null) {
+        if (receiver != null) {
             context.getApplicationContext().unregisterReceiver(receiver);
-            receiver=null;
+            receiver = null;
         }
     }
 
@@ -67,14 +73,56 @@ public class WifiSwitch extends AppWidgetProvider {
 
         // Construct the RemoteViews object
         RemoteViews views = new RemoteViews(context.getPackageName(), R.layout.wifi_switch);
-        if(wifiManager.isWifiEnabled())
-            views.setImageViewResource(R.id.imageButton,R.drawable.wifi_on);
+        if (wifiManager.isWifiEnabled())
+            views.setImageViewResource(R.id.imageButton, R.drawable.wifi_on);
         else
-            views.setImageViewResource(R.id.imageButton,R.drawable.wifi_off);
+            views.setImageViewResource(R.id.imageButton, R.drawable.wifi_off);
         //views.setTextViewText(R.id.appwidget_text, widgetText);
+        views.setOnClickPendingIntent(R.id.imageButton, getPendingSelfIntent(context, WIFI_CLICKED));
+
 
         // Instruct the widget manager to update the widget
         appWidgetManager.updateAppWidget(appWidgetId, views);
+    }
+
+    @Override
+    /**
+     * Receive click events to toogle wifi
+     */
+    public void onReceive(Context context, Intent intent) {
+        super.onReceive(context, intent);
+
+        if (WIFI_CLICKED.equals(intent.getAction())) {
+
+            AppWidgetManager appWidgetManager = AppWidgetManager.getInstance(context);
+
+            RemoteViews remoteViews = new RemoteViews(context.getPackageName(), R.layout.wifi_switch);
+            ComponentName            thisWidget = new ComponentName(context, WifiSwitch.class);
+
+            WifiManager wifiManager = (WifiManager) context.getSystemService(Context.WIFI_SERVICE);
+
+            // Update views
+            AppWidgetManager manager = AppWidgetManager.getInstance(context);
+            int[] appWidgetIds = manager.getAppWidgetIds(thisWidget);
+            final int N = appWidgetIds.length;
+            for (int i = 0; i < N; i++) {
+                if (!wifiManager.isWifiEnabled())
+                    remoteViews.setImageViewResource(R.id.imageButton, R.drawable.wifi_on);
+                else
+                    remoteViews.setImageViewResource(R.id.imageButton, R.drawable.wifi_off);
+            }
+
+            // Toogle wifi
+            wifiManager.setWifiEnabled(!wifiManager.isWifiEnabled());
+
+            appWidgetManager.updateAppWidget(thisWidget, remoteViews);
+        }
+    }
+
+    protected static PendingIntent getPendingSelfIntent(Context context, String action) {
+        Intent intent = new Intent(context, WifiSwitch.class);
+        intent.setAction(action);
+        return PendingIntent.getBroadcast(context, 0, intent, 0);
     }
 }
 
